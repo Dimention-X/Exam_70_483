89f18928acb0a14842faf1a784665a831d2b0ae0
Akhilesh Kumar Sahu <Akhileshsh028@outlook.com>
2017-06-20 15:01:42 +0530

    Listing 1-1 from the chapter -1

--- Chapter 1-1/Chapter 1-1/Program.cs
+++ Chapter 1-1/Chapter 1-1/Program.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Chapter_1_1
 {
     class Program
     {
+        public static void threadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("thread Methord calling : {0}", i);
+                Thread.Sleep(0);   // it will be paued for 1000 miliseconds after print proint 0 then goes to main methord for execution 
+                // and when that thread sleep then this thread will work this is synchronisation. two threads never be executed on the same portion of the program at a time.
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Thread t = new Thread(new ThreadStart(threadMethod));
+            t.Start();
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("mAIN mETHORD COUNT : {0}", i);
+                Thread.Sleep(500);
+            }
+            t.Join();
+
+
         }
     }
 }
