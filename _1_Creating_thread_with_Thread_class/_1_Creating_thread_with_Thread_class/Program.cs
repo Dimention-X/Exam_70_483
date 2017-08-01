ef93e52813e3c80d8c8f47c3afe476265111ed25
shubhamM <shubhamM@VS15UP3SM>
2017-08-01 07:02:57 +0000

    both threads run and print their message to the console. The Thread.Join method is called on the main thread to let it wait until the other thread finishes. Why the Thread.Sleep(0)? It is used to signal to Windows that this thread is finished. Instead of waiting for the whole time-slice of the thread to finish, it will immediately switch to another thread.

---
+++ _1_Creating_thread_with_Thread_class/_1_Creating_thread_with_Thread_class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _1_Creating_thread_with_Thread_class
+{
+    class Program
+    {
+        public static void ThreadMethord()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread Meth : "+ i);
+               
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethord));
+            t.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Main Mth : "+ i);
+                Thread.Sleep(0);
+            }
+
+            t.Join(); // waite for the above thread to complete 
+            Console.ReadLine();
+        }
+    }
+}
